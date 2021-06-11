7a3fb9735eccb53212411b74cd104dcc2b370a42
董敏超 <minchao.dong@melot.cn>
2021-06-11 11:41:04 +0800

    Initial commit

---
+++ list_eureka_server/src/main/java/com/list/list_eureka_server/ListEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.list.list_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ListEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListEurekaServerApplication.class, args);
+    }
+
+}
